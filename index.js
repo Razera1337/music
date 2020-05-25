730af0ca052f6e42708a1f7bd2582cd12b60762b
Ayaan Gaming <64188607+ayaan1005@users.noreply.github.com>
2020-05-25 21:05:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on('guildMemberAdd', async member => {
         try {
             const filter = m => {
                 if(m.author.bot) return;
-                if(m.author.id === member.id && m.content === `captcha`) return true;
+                if(m.author.id === member.id && m.content === captcha) return true;
                 else {
                     m.channel.send('You entered the captcha incorrectly.');
                     return false;
