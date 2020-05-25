a4515097f74340838963937c99b04b2083c31660
Ayaan Gaming <64188607+ayaan1005@users.noreply.github.com>
2020-05-25 21:40:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { MessageEmbed } = require('discord.js');
 const client = new Client();
 client.login(process.env.BOT_TOKEN);
 
-client.on('ready', () => console.log(`${bot.user.username} is online`))
-bot.user.setActivity("Hello", {type: "STREAMING", url:"https://twitch.tv/Strandable"});
+client.on('ready', () => console.log(`${client.user.username} is online`))
+client.user.setActivity("Hello", {type: "STREAMING", url:"https://twitch.tv/Strandable"});
 
 client.on('message', async message => {
     if(message.author.bot) return;
