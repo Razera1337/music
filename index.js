87b345c150c8cb518732ad2d46a2b2b9935b2d3a
Ayaan Gaming <64188607+ayaan1005@users.noreply.github.com>
2020-05-29 11:20:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Client } = require('discord.js');
 const { RichEmbed } = require('discord.js');
 const client = new Client();
 client.login(process.env.BOT_TOKEN);
+const guildInvites = new Map();
 
 client.on("ready", async () => {
     console.log(`${client.user.username} is online`)
@@ -34,4 +35,24 @@ client.on('message', async message => {
 
 client.on('guildMemberAdd', member => {
     console.log(member.user.tag);
+});
+
+client.on('guildMemberAdd', async member => {
+    const cachedInvites = guildInvites.get(member.guild.id);
+    const newInvites = await member.guild.fetchInvites();
+    guildInvites.set(member.guild.id, newInvites);
+    try {
+        const usedInvite = newInvites.find(inv => cachedInvites.get(inv.code).uses < inv.uses);
+        const embed = new RichEmbed()
+            .setDescription(`${member.user.tag} is the ${member.guild.memberCount} to join.\nJoined using ${usedInvite.inviter.tag}\nNumber of uses: ${usedInvite.uses}`)
+            .setTimestamp()
+            .setTitle(`${usedInvite.url}`);
+        const welcomeChannel = member.guild.channels.find(channel => channel.id === '715061320831074325');
+        if(welcomeChannel) {
+            welcomeChannel.send(embed).catch(err => console.log(err));
+        }
+    }
+    catch(err) {
+        console.log(err);
+    }
 });
