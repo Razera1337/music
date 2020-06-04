26f0ad9f2ebcab0ba1b991ccfc67fc1cc7047841
Ayaan Gaming <64188607+ayaan1005@users.noreply.github.com>
2020-06-04 20:32:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,23 +43,3 @@ client.on('guildMemberAdd', member => {
     console.log(member.user.tag);
 });
 
-client.on('guildMemberAdd', async member => {
-    const cachedInvites = guildInvites.get(member.guild.id);
-    const newInvites = await member.guild.fetchInvites();
-    guildInvites.set(member.guild.id, newInvites);
-    try {
-        const usedInvite = newInvites.find(inv => cachedInvites.get(inv.code).uses < inv.uses);
-        const embed = new RichEmbed()
-            .setColor("Green")
-            .setDescription(`**${member.user.tag}** is the **${member.guild.memberCount}** to join.\n\nJoined using **${usedInvite.inviter.tag}**\n\nNumber of uses: **${usedInvite.uses}**`)
-            .setTimestamp()
-            .setTitle(`${usedInvite.url}`);
-        const welcomeChannel = member.guild.channels.find(channel => channel.id === '715061320831074325');
-        if(welcomeChannel) {
-            welcomeChannel.send(embed).catch(err => console.log(err));
-        }
-    }
-    catch(err) {
-        console.log(err);
-    }
-});
