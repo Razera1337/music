8b753241b2c0f6ce7c4dcbee20910f6bf8777144
Glitch (musivgaf) <none>
2020-07-25 10:36:22 +0000

    👟📹 Updated with Glitch

--- events/clientEvents/ready.js
+++ events/clientEvents/ready.js
@@ -2,9 +2,6 @@
 module.exports = {
   name: "ready",
   async execute(client) {
-    client.user.setActivity(`!help | 🎶`, {
-      type: "STREAMING",
-    });
     console.log(`- Activated -`);
   },
 };
