0f363f30e7095ff896be42697a78d1ee55b96c72
Ayaan Gaming <64188607+ayaan1005@users.noreply.github.com>
2020-08-10 18:53:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT)
+
 const MusicClient = require("./struct/client.js");
 const client = new MusicClient({});
