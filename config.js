6e4abb32fe88d9c52f386e439d111d36ff6d3c02
Ayaan Gaming <64188607+ayaan1005@users.noreply.github.com>
2020-08-22 21:04:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require("dotenv/config");
 
 module.exports = {
-  token: "Njk4NDk3MTkyOTM1MTYxODY2.XpGsNA.lF-Zpx-XragQymopCH7MkRVqsb4",
+  token: "Njk4NDk3MTkyOTM1MTYxODY2.XpGsNA.iqh_67zYjmtMvYP9MVhou-0tFzA",
   api_key: "AIzaSyDQGuX9dXBM9uKKeL0wzxeDGpwLFUrTNng",
   embedColor: "#e08512",
   prefix: "!!",
